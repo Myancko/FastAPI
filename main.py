f7db80af5b068eb1ad1a44ef4a85b2e0607ad0f0
Jonathan Nicolas <weptpear394@gmail.com>
2023-10-03 17:45:35 -0300

    adaptei o codigo para que atendesse aos requisitos da atividade

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request, BackgroundTasks
+from datetime import datetime 
 from pydantic import BaseModel
 from typing import List, Annotated
 import models
+import time
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session 
 
@@ -11,7 +13,7 @@ models.Base.metadata.create_all(bind=engine)
 class Aluno_data (BaseModel):
     name: str
     matricula: str
-
+    
 def get_db ():
     db = SessionLocal()
     
@@ -20,11 +22,36 @@ def get_db ():
     finally:
         db.close()
         
-        
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class Log:
+
+    def database_log_injection(self, request, db):
+
+        rq= request.method
+        db_log = models.log(data = datetime.now(), requisition = str(rq),  user = 'anon')
+        db.add(db_log)
+        db.commit()
+        print('backgroud funcinou')
+        
+    
+@app.middleware("http")
+async def log_general(request: Request, call_next):
+
+    method_name = request.method
+    qp_map = request.query_params
+    pp_map = request.path_params
+    with open("request_log.txt", mode="a") as reqfile:
+        content = f"method: '{method_name}', query param: '{qp_map}', path params: '{pp_map}' received at '{datetime.now()}'\n"
+        reqfile.write(content)
+    response = await call_next(request)
+
+    return response
+
 @app.get("/cadastro/{id}")
-async def read_student ( student_id: int, db: db_dependency ):
+async def read_student ( student_id: int, db: db_dependency, request : Request, bg_task: BackgroundTasks ):
+    log = Log()
+    bg_task.add_task(log.database_log_injection, request=request, db=db)
     result = db.query(models.Aluno).filter(models.Aluno.id == student_id).first()
     
     if not result :
@@ -32,14 +59,23 @@ async def read_student ( student_id: int, db: db_dependency ):
     return  result
 
 @app.post("/cadastro/")
-async def create_question (aluno: Aluno_data, db: db_dependency):
+async def create_question (aluno: Aluno_data, db: db_dependency, request : Request, bg_task: BackgroundTasks):
     db_aluno =models.Aluno(name=aluno.name, matricula=aluno.matricula)
     db.add(db_aluno)
+    
+    log = Log()
+    
+    bg_task.add_task(log.database_log_injection, request=request, db=db)
+    
     db.commit()
+    print('ok')
     db.refresh(db_aluno)
 
+
 @app.get("/cadastro/")
-async def read_student ( db: db_dependency ):
+async def read_student ( db: db_dependency, request : Request, bg_task: BackgroundTasks ):
+    log = Log()
+    bg_task.add_task(log.database_log_injection, request=request, db=db)
     result = db.query(models.Aluno).all()
     
     if not result :
@@ -48,7 +84,9 @@ async def read_student ( db: db_dependency ):
     return result
 
 @app.delete("/cadastro/{id}")
-async def read_student ( student_id: int, db: db_dependency ):
+async def read_student ( student_id: int, db: db_dependency, request : Request, bg_task: BackgroundTasks ):
+    log = Log()
+    bg_task.add_task(log.database_log_injection, request=request, db=db)
     result = db.query(models.Aluno).filter(models.Aluno.id == student_id).delete()
     db.commit()
     if not result :
@@ -57,7 +95,9 @@ async def read_student ( student_id: int, db: db_dependency ):
     return  result,  '200'
 
 @app.patch("/cadastro/{id}")
-async def update_student(student_id: int, aluno_data: Aluno_data, db: db_dependency):
+async def update_student(student_id: int, aluno_data: Aluno_data, db: db_dependency, request : Request, bg_task: BackgroundTasks):
+    log = Log()
+    bg_task.add_task(log.database_log_injection, request=request, db=db)
     db_aluno = db.query(models.Aluno).filter(models.Aluno.id == student_id).first()
     
     if not db_aluno:
--- models.py
+++ models.py
@@ -8,3 +8,12 @@ class Aluno (Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     matricula = Column(String, index=True)
+    
+class log (Base):
+    
+    __tablename__ = 'sys_log'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    data = Column(String, index=True)
+    requisition  = Column(String, index=True)
+    user = Column(String, index=True)
